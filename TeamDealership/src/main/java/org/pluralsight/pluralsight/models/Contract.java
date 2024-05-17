6c1a6883b9e48b36c1f74f46a1dad3358159eda9
flour <99916123+Flour510@users.noreply.github.com>
2024-05-17 10:48:56 -0700

    Update

--- TeamDealership/src/main/java/org/pluralsight/pluralsight/models/Contract.java
+++ TeamDealership/src/main/java/org/pluralsight/pluralsight/models/Contract.java
@@ -2,4 +2,59 @@ package org.pluralsight.pluralsight.models;
 
 public abstract class Contract
 {
+    // variables
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    // constructor
+    public Contract(String date, String customerName, String customerEmail, String vehicleSold, double totalPrice, double monthlyPayment) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    // getters and setters
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+
 }
--- TeamDealership/src/main/java/org/pluralsight/pluralsight/models/SalesContract.java
+++ TeamDealership/src/main/java/org/pluralsight/pluralsight/models/SalesContract.java
@@ -2,5 +2,60 @@ package org.pluralsight.pluralsight.models;
 
 public class SalesContract extends Contract
 {
+    // variables
+    private final double salesTax;
+    private final double recordingFee;
+    private double processingFee;
+    private boolean finance;
 
+    // constructor
+    public SalesContract(String date, String customerName, String customerEmail, String vehicleSold, double totalPrice, double monthlyPayment, double salesTax, double recordingFee, double processingFee, boolean finance) {
+        super(date, customerName, customerEmail, vehicleSold, totalPrice, monthlyPayment);
+        this.salesTax = salesTax;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.finance = finance;
+    }
+
+    // getters and setters
+    public double getSalesTax() {
+        return salesTax;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public double getProcessingFee() {
+
+        if(Vehicle.getPrice)
+        {
+
+        }
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
+    @Override
+    public double getPrice()
+    {
+
+    }
+
+    @Override
+    public double getMonthlyPayment()
+    {
+
+    }
 }
